d072859fb4449e09314041948f5e4284e5ba1fa0
Karthik Manickam <karthik.omanickam@gmail.com>
2018-09-24 10:40:22 -0700

    File Config

---
+++ src/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className="layout">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default Layout;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './Layout';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Layout />, document.getElementById('root'));
+registerServiceWorker();
