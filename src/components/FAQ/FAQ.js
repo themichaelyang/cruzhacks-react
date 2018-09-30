fbfdd32a639a00b32dfc617a77810c97841ed277
Karthik Manickam <karthik.omanickam@gmail.com>
2018-09-29 19:55:52 -0700

    Added FAQ directory

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FAQ extends Component {
+  render() {
+    return (
+      <div className="faq-container">
+        
+      </div>
+    );
+  }
+}
+
+export default FAQ;
