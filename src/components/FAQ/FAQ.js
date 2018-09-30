9ce4975586d1024deabb2c71ffe9ca84aaf2a039
Karthik Manickam <karthik.omanickam@gmail.com>
2018-09-30 10:52:55 -0700

    Added stylesheet and markup around FAQ section

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Question from './Question';
 
 class FAQ extends Component {
   render() {
     return (
       <div className="faq-container">
-        
+        <Question />
       </div>
     );
   }
---
+++ src/components/FAQ/Question.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Question extends Component {
+  render() {
+    return (
+      <div className="question">
+        <span className="question__title">{this.props.title}</span>
+        <p className="question__answer">{this.props.answer}</p>
+      </div>
+    );
+  }
+}
+
+export default Question;
