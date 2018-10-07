49c44e57d0cfaf66da54a7b5dce502e68f9cc06f
Karthik Manickam <karthik.omanickam@gmail.com>
2018-10-06 18:16:18 -0700

    Hero refactor

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,15 +6,11 @@ class Hero extends Component {
   render() {
     return (
       <div className="hero">
-        <img className="logo" src={logo} alt="logo"/>
-        <div className="hero__text">
-          <h1 className="hero__text__header">CruzHacks 2019</h1>
-          <p className="hero__text__quote">Create something that makes a difference.</p>
-          <span className="hero__text__location">Porter Dining Hall, UC Santa Cruz</span>
-          <span className="hero__text__location">January 19-21, 2019</span>
-          <a href="#" className="hero__text__cta">Apps Open In Dec</a>
-        </div>
-        <img className="hero__bg" src={hero} alt="logo"/>        
+        <img className="hero__logo" src={logo} alt="logo"/>
+        <h1 className="hero__title">CruzHacks 2019</h1>
+        <span className="hero__text">Create something that makes a difference.</span>
+        <a href="#" className="hero__apps">Apps open in Nov</a>
+        <img src={hero} alt="" className="hero__bg"/>
       </div>
     );
   }
