67c873b51461897086b0d08d8dc74e78625e7527
Karthik Manickam <karthik.omanickam@gmail.com>
2018-10-09 13:40:42 -0700

    Added date and location to landing

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,6 +9,7 @@ class Hero extends Component {
         <img className="hero__logo" src={logo} alt="logo"/>
         <h1 className="hero__title">CruzHacks 2019</h1>
         <span className="hero__text">Create something that makes a difference.</span>
+        <span className="hero__text__alt">Jan 19-21, 2019 @ Stevenson Event Center</span>
         <a href="#" className="hero__apps">Apps open in Nov</a>
         <img src={hero} alt="" className="hero__bg"/>
       </div>
