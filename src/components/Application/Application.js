55309ce2bb5d15440723da6c363212fd5007ea03
Karthik Manickam <karthik.omanickam@gmail.com>
2018-10-24 14:30:07 -0700

    Created HackerForm component

--- src/components/Application/Application.js
+++ src/components/Application/Application.js
@@ -8,26 +8,22 @@ class Application extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      application: 1
+      application: 0
     }
   }
 
   toggleApplication = (event) => {
     let app = event.target.dataset.app
-    this.setState({application: app}, function() {
-      console.log(this.state.application)
-    })
+    this.setState({application: app})
   }
 
   render() {
     return (
       <div className="application">
-        <NavLink to="/"><img src={logo} className="application__logo"/></NavLink>
+        <NavLink to="/"><img src={logo} alt="logo" className="application__logo"/></NavLink>
         <h1 className="application__title">CruzHacks 2019 Applications</h1>
         <div className="application__select">
-          <div className="separator">
-            <span className="application__select__title">Choose an Application Type</span>
-          </div>
+          <span className="application__select__title">Choose an Application Type</span>
           <AppSelect name="Hacker" class={this.state.application == 1 ? 'selected' : ''} data="1" handler={this.toggleApplication}/>
           <AppSelect name="Mentor" class={this.state.application == 2 ? 'selected' : ''} data="2" handler={this.toggleApplication}/>
           <AppSelect name="Judge" class={this.state.application == 3 ? 'selected' : ''} data="3" handler={this.toggleApplication}/>
---
+++ src/components/Application/HackerForm/HackerForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class HackerForm extends Component {
+  render() {
+    return (
+      <div className="hackerform">
+      
+      </div>
+    );
+  }
+}
+
+export default HackerForm;
