4535c0618eff9fdf9bd8d97dc0f23f03377de933
Karthik Manickam <karthik.omanickam@gmail.com>
2018-10-24 20:39:29 -0700

    Changes to application styling

--- src/components/Application/Application.js
+++ src/components/Application/Application.js
@@ -3,6 +3,7 @@ import logo from 'assets/logo.svg';
 import {NavLink} from 'react-router-dom';
 
 import AppSelect from './AppSelect';
+import HackerForm from './HackerForm';
 
 class Application extends Component {
   constructor(props) {
@@ -13,8 +14,19 @@ class Application extends Component {
   }
 
   toggleApplication = (event) => {
-    let app = event.target.dataset.app
+    let app = parseInt(event.target.dataset.app)
     this.setState({application: app})
+  }
+
+  renderForm(application) {
+    console.log(typeof application)
+    switch (application) {
+      case 1: console.log('hackerform') 
+      return <HackerForm />
+      case 2: return <h1>Mentor</h1>
+      case 3: return <h1>Judge</h1>
+      case 4: return <h1>Volunteer</h1>      
+    }
   }
 
   render() {
@@ -29,6 +41,7 @@ class Application extends Component {
           <AppSelect name="Judge" class={this.state.application == 3 ? 'selected' : ''} data="3" handler={this.toggleApplication}/>
           <AppSelect name="Volunteer" class={this.state.application == 4 ? 'selected' : ''} data="4" handler={this.toggleApplication}/>
         </div>
+        {this.renderForm(this.state.application)}
       </div>
     );
   }
--- src/components/Application/HackerForm/HackerForm.js
+++ src/components/Application/HackerForm/HackerForm.js
@@ -3,8 +3,15 @@ import React, {Component} from 'react';
 class HackerForm extends Component {
   render() {
     return (
-      <div className="hackerform">
-      
+      <div className="form">
+        <h2 className="form__title">Hacker Application</h2>
+        <form action="#" className="hacker__form">
+          <input type="text" className="form__input" name="firstname" placeholder="firstname" required/>
+          <input type="text" className="form__input" name="lastname" placeholder="lastname" required/>
+          <input type="email" className="form__input" name="email" placeholder="email" required/>
+          <input type="tel" className="form__input" name="phone" placeholder="phone number"/>
+          <input type="submit" className="form__submit"/>                
+        </form>
       </div>
     );
   }
