915af4b405940e831bd964025ce6987b7b00ba1a
Karthik Manickam <karthik.omanickam@gmail.com>
2018-11-01 21:48:55 -0700

    Added TShirt select

---
+++ src/components/Application/JudgeForm/Availability/Availability.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+class Availability extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      availability: ''
+    }
+  }
+
+  changeValue = (event) => {
+    this.setState({availability: event.target.dataset.availability}, function() {
+      this.props.handler({availability: this.state.availability})
+    })
+  }
+
+  render() {
+    return (
+      <div className="availability">        
+        <h2 className="availability__title">Will you be free from 12:00 - 2:00 pm on Sunday January 20th, 2019?</h2>
+        <div className="availability__select">
+          <span className={this.state.availability == "Yes" ? "availability__select__option active" : "availability__select__option"} data-availability="Yes" onClick={this.changeValue}>Yes</span>
+          <span className={this.state.availability == "No" ? "availability__select__option active" : "availability__select__option"} data-availability="No" onClick={this.changeValue}>No</span>           
+          <input type="hidden" name="availability" className="availability__value" onChange={this.props.handler} value={this.state.availability}/>
+        </div>        
+      </div>
+    );
+  }
+
+}
+
+export default Availability;
---
+++ src/components/Application/TShirtSelect/TShirtSelect.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class TShirtSelect extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      shirt: ''
+    }
+  }
+
+  changeValue = (event) => {
+    this.setState({shirt: event.target.dataset.shirt}, function() {
+      this.props.handler({shirt: this.state.shirt})
+    })
+  }
+
+  render() {
+    return (
+      <div className="shirt">        
+        <h2 className="shirt__title">What's your T-Shirt size? (Unisex)</h2>
+        <div className="shirt__select">
+          <span className={this.state.shirt == "XS" ? "shirt__select__option active" : "shirt__select__option"} data-shirt="XS" onClick={this.changeValue}>XS</span>
+          <span className={this.state.shirt == "S" ? "shirt__select__option active" : "shirt__select__option"} data-shirt="S" onClick={this.changeValue}>S</span>   
+          <span className={this.state.shirt == "M" ? "shirt__select__option active" : "shirt__select__option"} data-shirt="M" onClick={this.changeValue}>M</span>   <span className={this.state.shirt == "L" ? "shirt__select__option active" : "shirt__select__option"} data-shirt="L" onClick={this.changeValue}>L</span>
+          <span className={this.state.shirt == "XL" ? "shirt__select__option active" : "shirt__select__option"} data-shirt="XL" onClick={this.changeValue}>XL</span>   
+          <span className={this.state.shirt == "XXL" ? "shirt__select__option active" : "shirt__select__option"} data-shirt="XXL" onClick={this.changeValue}>XXL</span>                   
+          <input type="hidden" name="shirt" className="shirt__value" onChange={this.props.handler} value={this.state.shirt}/>
+        </div>        
+      </div>
+    );
+  }
+}
+
+export default TShirtSelect;
