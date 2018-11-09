49e42d91ed2ce10fab0228c33a0539bb621c6a98
greg-wolff <gregwwolff@gmail.com>
2018-11-08 23:08:54 -0800

    remove mlh banner on mobile

--- src/Layout.js
+++ src/Layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from 'styled-components';
 
 import Landing from 'components/Landing';
 import Application from 'components/Application';
@@ -7,11 +8,19 @@ import Application from 'components/Application';
 import 'styles/css/normalize.css';
 import 'styles/sass/base.css';
 
+const GlobalStyle = createGlobalStyle`
+  #mlh-trust-badge {
+    @media(max-width: 600px) {
+      display: none !important;
+    }
+  }
+`
 
 class Layout extends Component {
   render() {
     return (
       <div className="layout">
+        <GlobalStyle />
         <Router>
           <Switch>
             <Route path="/d7o59mpc" exact component={Application}/>
