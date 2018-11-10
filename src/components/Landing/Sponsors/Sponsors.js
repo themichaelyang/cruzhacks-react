0bd3587f27f2efd7262a78d4b85779c0347d902a
mocklee <emockler@ucsc.edu>
2018-11-09 23:42:58 -0800

    added SCTB & UCSC Arts logo, fixed logo wrap

--- src/components/Landing/Sponsors/Sponsors.js
+++ src/components/Landing/Sponsors/Sponsors.js
@@ -9,30 +9,37 @@ import PlantronicsKilo from 'assets/sponsors/Plantronics-KILO/PLT_Lava-box_Logo_
 import ProductOpsKilo from 'assets/sponsors/ProductOps-KILO/grey and white versions/vertical_white.svg';
 import SouthSwellKilo from 'assets/sponsors/South Swell Ventures-KILO/SouthSwellVentures_logo-2.png'
 import ChooseSCCenti from 'assets/sponsors/Choose Santa Cruz-CENTI/ChooseSantaCruz-Diamond Artwork.png'
-import MLHPartner from 'assets/sponsors/MLH - Partner/mlh-logo-white.svg'
+import MLHPartner from 'assets/sponsors/MLH - PARTNER/mlh-logo-white.svg'
 import ChallengeRocketPartner from 'assets/sponsors/Challenge Rocket - PARTNER/challengerocket_logo_color_rgb.png'
+import UCSCArtsKilo from 'assets/sponsors/UCSC Arts Division - KILO/artsdivision-logo.png'
+import SCTBPartner from 'assets/sponsors/Santa Cruz Tech Beat - PARTNER/SCTB-logo.png'
 
 class Sponsors extends Component {
   render() {
     return (
      <div className="sponsors__container"> 
       <h2 className="sponsors__title">Thank You to Our Wonderful Sponsors</h2>
-      <div className="sponsors__logos giga">
+      <div className="sponsors__logos gigas">
         <div className="sponsors__giga">
           <a href="https://graddiv.ucsc.edu" target = "_blank">
-          <img src={UCSCGradDivGiga} alt="UCSC Graduate Division" height="150px" width="450px"/>
+          <img src={UCSCGradDivGiga} alt="UCSC Graduate Division" height="200px" width="500px"/>
           </a>
         </div>
         <div className="sponsors__giga">
           <a href="https://aws.amazon.com/education/awseducate/" target = "_blank">
-          <img src={AmazonGiga} alt="Amazon" height="150px" width="450px"/>
+          <img src={AmazonGiga} alt="Amazon" height="200px" width="500px"/>
           </a>
         </div>
       </div>          
-      <div className="sponsors__logos kilo">
+      <div className="sponsors__logos kilos">
         <div className="sponsors__kilo">
           <a href="https://www.bsoe.ucsc.edu" target = "_blank">
           <img src={BSOEKilo} alt="Baskin School of Engineering" height="100px" width="225px"/>
+          </a>
+        </div>
+        <div className="sponsors__kilo">
+          <a href="http://arts.ucsc.edu" target = "_blank">
+          <img src={UCSCArtsKilo} alt="UCSC Arts" height="100px" width="225px"/>
           </a>
         </div>
         <div className="sponsors__kilo">
@@ -56,7 +63,7 @@ class Sponsors extends Component {
           </a>
         </div>
       </div>
-      <div className="sponsors__logos centi">
+      <div className="sponsors__logos centis">
         <div className="sponsors__centi">
           <a href="https://choosesantacruz.com/" target="_blank">
           <img src={ChooseSCCenti}  alt="Choose Santa Cruz" height="100px"/>
@@ -64,15 +71,20 @@ class Sponsors extends Component {
         </div>
       </div>
       <h2 className="sponsors__title">and Our Great Partners</h2>
-      <div className="sponsors__logos centi">
-        <div className="sponsors__kilo">
+      <div className="sponsors__logos partners">
+        <div className="sponsors__partner">
           <a href="https://mlh.io/" target="_blank">
-          <img src={MLHPartner}  alt="Major League Hacking" height="110px"/>
+          <img src={MLHPartner}  alt="Major League Hacking" width="225px" height="110px"/>
           </a>
         </div>
-        <div className="sponsors__kilo">
+        <div className="sponsors__partner">
           <a href="https://challengerocket.com/" target="_blank">
-          <img src={ChallengeRocketPartner}  alt="Challenge Rocket" height="110px"/>
+          <img src={ChallengeRocketPartner}  alt="Challenge Rocket" width="225px" height="110px"/>
+          </a>
+        </div>
+        <div className="sponsors__partner">
+          <a href="http://santacruztechbeat.com/" target="_blank">
+          <img src={SCTBPartner}  alt="Santa Cruz Tech Beat" width="225px" height="110px"/>
           </a>
         </div>
       </div>
